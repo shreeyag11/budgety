351fe3db89e38e009f36c7c68c79e94cb4f116eb
Shreeya Gupta <shreeyag11@gmail.com>
2020-05-21 16:17:46 -0400

    data structures created in budgetCtrl

--- app.js
+++ app.js
@@ -1,33 +1,103 @@
 //BUDGET CONTROLLER
 var budgetController = (function () {
-    var addData = function(description, value) {
-        var desc = description;
-        var val = value;
-    }
-    console.log("Value added:"+value);
+
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
 
     return {
-        publicAddData: function(description, value) {
-            addData(description, value);
+        addItems: function (type, desc, val) {
+            var ID, newItem;
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length-1].id + 1;
+            }
+            else {
+                ID = 0;
+            }
+            if (type === 'exp') {
+                newItem = new Expense(ID, desc, val);
+            }
+            else if (type === 'inc') {
+                newItem = new Income(ID, desc, val)
+            }
+            data.allItems[type].push(newItem);
+            return newItem;
         }
     }
+
 })();
 
 //UI CONTROLLER
 var UIController = (function () {
 
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    };
+
+    return {
+        getinput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    }
 })();
 
-//CONTROLLER
+//GLOBAL CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-    document.querySelector('.add__btn').addEventListener('click', function() {
-    console.log("hii");
-    var description = document.querySelector('.add__description').value;
-    var value = document.querySelector('.add__value').value;
-    budgetCtrl.publicAddData(description, value);
 
-    });
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDOMstrings();
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    }
+
+    var ctrlAddItem = function () {
+        var input = UICtrl.getinput();
+        var data = budgetCtrl.addItems(input.type, input.description, input.value);
+        console.log(data);
+    }
+
+    return {
+        init: function () {
+            console.log("Application started");
+            setupEventListeners();
+        }
+    }
 })(
-  budgetController,
-  UIController
+    budgetController,
+    UIController
 );
+
+controller.init();
